59aeac5a8b33e5f4c62cc040eb1da4ece60efa7b
Jamie <jamie@kentdogwalker.co.uk>
2024-04-05 18:21:18 +0100

    Updates

--- resources/views/hotel/rooms/layout.blade.php
+++ resources/views/hotel/rooms/layout.blade.php
@@ -14,7 +14,7 @@
 
 <body class="full-screen-big" style="background-color: black; color: white; margin: 0; height: 100vh; display: flex; align-items: top; justify-content: center;">
 
-    <div class="full-screen-container" style="width: 100%; max-width: 100%; padding: 20px;">
+    <div class="full-screen-container" style="width: 100%; max-width: 100%; padding: 40px;">
         @yield('content')
     </div>
 
--- resources/views/hotel/rooms/show.blade.php
+++ resources/views/hotel/rooms/show.blade.php
@@ -3,29 +3,39 @@
 @section('content')
 <div class="row" style="display: flex; width: 100%;">
     <!-- Left Column 75% -->
-    <div style="flex: 75%;">
+    <div style="flex: 70%;">
         <!-- Content for the left column goes here -->
-        <h1>{{ $room->RoomName }}</h1>
-        <p>SUITE</p>
+        <h1 class="display-3 text-uppercase mb-0 pb-0">AMSTERDAM</h1>
+        <h1 style="color: #E6348B"" class="text-uppercase display-6 mt-0 pt-0">SUITE</h1>
     </div>
     <!-- Right Column 25% -->
-    <div style="flex: 25%;">
+    <div class="align-top" style="flex: 30%;">
         <!-- Content for the right column goes here -->
         <img src="{{ asset('assets') }}/img/rooms/london.png" width="100%">
     </div>
 </div>
 
 <!-- Row 2 -->
-<div class="row" style="display: flex; width: 100%;">
+<div class="row mt-5" style="display: flex; width: 100%;">
 
-    <div style="flex: 20%;">
+    <div
+        class="bg-image border"
+        style="
+        flex: 20%;
+        background-image: url('{{ asset('assets') }}/img/dogs/arrow.jpg');
+        height: 200px;
+
+        ">
         <!-- Content for the left column goes here -->
-        <img src="{{ asset('assets') }}/img/dogs/arrow.jpg" width="100%">
+
     </div>
 
-    <div style="flex: 40%;">
+    <div style="flex: 40%;" class="ml-3">
         <!-- Content for the right column goes here -->
-        Dog Name & Info
+        <h1 class="display-4 text-uppercase">Rex</h1>
+        <p>Boxer</p>
+        <p>4 Years Old</p>
+
     </div>
 
     <div style="flex: 20%;">
@@ -34,5 +44,6 @@
     </div>
 </div>
 
+<!-- <h1 class="display-1 text-uppercase">{{ $room->RoomName }}</h1> -->
 
 @endsection
